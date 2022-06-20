4a6017660fa13f1a6b5d1523a62ad9a6ab78e134
Gambu101 <100051823+Gambu101@users.noreply.github.com>
2022-06-20 20:25:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+let me = { name: "Ohalete Gabriel", height: "6ft 2inches", country: "Nigeria" };
+
+document.getElementById('demo').innerHTML = "Name: "+ me.name  + ", Height: "+ me.height +", Country: "+me.country
